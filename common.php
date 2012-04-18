9fb1dab66d63930608fe1f952152185da08aa3c6
Wilson Zeng <jackzcs@gmail.com>
2012-04-18 14:32:55 +0800

    fix bug about loading config.php

--- common.php
+++ common.php
@@ -1,7 +1,12 @@
 <?php
+//zcs=>[Fix Bug]这里没有include config.php后面对$_IMC也失效; 另外,之前$_IMC中enable,debug配置是无效的
+include_once('config.php');
+if (!$_IMC['enable']) {
+	exit(0);
+}
+//<=zcs
 
 $im_version = '@VERSION';
-#$_IMC['debug'] = true;
 
 if ( !defined( 'WEBIM_PATH' ) ) 
 	define( 'WEBIM_PATH', dirname( __FILE__ ) . '/' );
@@ -10,6 +15,7 @@ if ( !defined( 'WEBIM_PATH' ) )
 if ( version_compare( PHP_VERSION, '4.3', '<' ) ) {
 	die( sprintf( 'Your server is running PHP version %s but webim requires at least 4.3', PHP_VERSION ) );
 }
+
 // Modify error reporting levels to exclude PHP notices
 if( isset( $_IMC['debug'] ) ) {
 	error_reporting( -1 );
@@ -58,24 +64,20 @@ require_once( WEBIM_PATH . 'interface.php' );
  * $im_params = array_merge( $_GET, $_POST );
  */
 
-
-function webim_initdb(){
-	global $imdb, $_IMC;
-
-	/** $imdb, $imuser, $imclient, $_IMC */
-	$imdb = new webim_db( $_IMC['dbuser'], $_IMC['dbpassword'], $_IMC['dbname'], $_IMC['dbhost'] );
-
-	// Die if MySQL is not new enough
-	if ( $imdb->ready && version_compare($imdb->db_version(), '4.1.2', '<') ) {
-		die( sprintf( 'WebIM requires MySQL 4.1.2 or higher' ) );
-	}
-
-	$imdb->set_prefix( $_IMC['dbtable_prefix'] );
-
-	$imdb->add_tables( array( 'webim_settings', 'webim_histories', 'webim_users' ) );
+if( $_IMC["host_from_domain"] ){
+	$_IMC["host"] = $_SERVER['HTTP_HOST'];
 }
 
-webim_initdb();
+/** $imdb, $imuser, $imclient, $_IMC */
+$imdb = new webim_db( $_IMC['dbuser'], $_IMC['dbpassword'], $_IMC['dbname'], $_IMC['dbhost'] );
+
+// Die if MySQL is not new enough
+if ( version_compare($imdb->db_version(), '4.1.2', '<') ) {
+	die( sprintf( 'WebIM requires MySQL 4.1.2 or higher' ) );
+}
+
+$imdb->set_prefix( $_IMC['dbtable_prefix'] );
+$imdb->add_tables( array( 'webim_settings', 'webim_histories' ) );
 
 $imticket = webim_gp( 'ticket' );
 if( $imticket ) {
